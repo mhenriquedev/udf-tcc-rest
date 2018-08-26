fb423d7078a84198bfb5403f8289304595e5124d
Mateus Henrique <mateus_twister@hotmail.com>
2018-08-26 13:22:09 -0300

    Ajustes no controller

--- src/main/java/com/udf/tcc/controllers/CriancaController.java
+++ src/main/java/com/udf/tcc/controllers/CriancaController.java
@@ -17,27 +17,31 @@ public class CriancaController {
 	CriancaRepository criancaRepository;
 
 	// Busca todas as criancas
+	@CrossOrigin(origins = "http://localhost:5000")
 	@GetMapping("/criancas")
-	public List<Crianca> getAllNotes() {
+	public List<Crianca> getAllCriancas() {
 		return criancaRepository.findAll();
 	}
 
 	// Cria uma nova crianca
+	@CrossOrigin(origins = "http://localhost:5000")
 	@PostMapping("/criancas")
 	public Crianca createNote(@Valid @RequestBody Crianca crianca) {
 		return criancaRepository.save(crianca);
 	}
 
 	// Busca uma crianca por id
+	@CrossOrigin(origins = "http://localhost:5000")
 	@GetMapping("/criancas/{id}")
-	public Crianca getNoteById(@PathVariable(value = "id") Long criancaId) {
+	public Crianca getCriancaPorId(@PathVariable(value = "id") Long criancaId) {
 		return criancaRepository.findById(criancaId)
 				.orElseThrow(() -> new ResourceNotFoundException("Crianca", "id", criancaId));
 	}
 
 	// Atualiza os detalhes da crianca
+	@CrossOrigin(origins = "http://localhost:5000")
 	@PutMapping("/criancas/{id}")
-	public Crianca updateNote(@PathVariable(value = "id") Long criancaId, @Valid @RequestBody Crianca criancaDetails) {
+	public Crianca atualizaCrianca(@PathVariable(value = "id") Long criancaId, @Valid @RequestBody Crianca criancaDetails) {
 
 		Crianca crianca = criancaRepository.findById(criancaId)
 				.orElseThrow(() -> new ResourceNotFoundException("Note", "id", criancaId));
@@ -51,7 +55,8 @@ public class CriancaController {
 
 	// Deleta uma crianca
 	@DeleteMapping("/criancas/{id}")
-	public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long criancaId) {
+	@CrossOrigin(origins = "http://localhost:5000")
+	public ResponseEntity<?> deletaCrianca(@PathVariable(value = "id") Long criancaId) {
 		Crianca crianca = criancaRepository.findById(criancaId)
 				.orElseThrow(() -> new ResourceNotFoundException("Note", "id", criancaId));
 
